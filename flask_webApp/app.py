923b0db6bb3368cc943c76ac31c4198c9463827f
amin0123 <lamloumamin01@gmail.com>
2021-08-01 06:11:58 -0700

    new new

--- flask_webApp/app.py
+++ flask_webApp/app.py
@@ -4,29 +4,34 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 app = Flask(__name__)
-case =""
-toarduino=""
+case = ""
+toarduino = ""
+
+
 @app.route('/')
 @app.route('/on2')
 def on():
-    if toarduino = "<ledon>":
+    if (serial.in_waiting > 0):
+        fromarduino = ser.readline()
+    if (fromarduino == "ledon"):
+        toarduino = "<ledoff>"
+        case = "False"
+    elif (fromarduino == "ledoff"):
+        toarduino = "<ledon>"
         case = "True"
-    elif toarduino
-        case ="False"
-    
+
     toarduinoencoded = toarduino.encode()
     ser.write(toarduinoencoded)
-    return render_template('on2.html',case=case)
+    return render_template('on2.html', case=case)
 
 
-@app.route('/off2')
-def off():
-    toarduino2 = "<ledoff>"
-    toarduinoencoded2 = toarduino2.encode()
-    ser.write(toarduinoencoded2)
-    return render_template('off2.html')
+# @app.route('/off2')
+# def off():
+ #   toarduino2 = "<ledoff>"
+  #  toarduinoencoded2 = toarduino2.encode()
+   # ser.write(toarduinoencoded2)
+    # return render_template('off2.html')
 
 
- 
 if __name__ == '__main__':
-    app.run(debug = True)   
+    app.run(debug=True)
